2cca5ef31bf7bedae330381776cc596c7948dbf3
evgnanii <nikonovich.evgen@gmail.com>
2020-05-29 17:29:50 +0300

    Added Courier and Comment

--- core/src/main/java/by/st/deliver/core/config/CoreConfiguration.java
+++ core/src/main/java/by/st/deliver/core/config/CoreConfiguration.java
@@ -1,4 +1,4 @@
-package by.st.deliver.core;
+package by.st.deliver.core.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
